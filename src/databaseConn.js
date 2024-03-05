4db431d215525189b871517df38c40822cf2fa96
dwarkesh 99 <deoredwarkesh36@gmail.com>
2024-03-05 19:57:52 +0530

    update in app.js file

--- src/databaseConn.js
+++ src/databaseConn.js
@@ -6,5 +6,5 @@ main().catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/users-register');
-  console.log("connect successfully")
+  console.log("connect successfully") 
 }
